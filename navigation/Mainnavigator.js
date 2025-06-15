e3ef711b698b2c30dcf96cd89f591975dbcfa75c
Ricardo777 <ricardosgithub@gmail.com>
2025-06-15 17:42:49 -0600

    funcionalidades

--- navigation/Mainnavigator.js
+++ navigation/Mainnavigator.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import HomeScreen from "../pages/HomeScreen";
+import CartScreen from "../pages/CartScreen";
+import ProductDetailsScreen from "../pages/ProductDetailsScreen";
+
+
+
 const Stack = createStackNavigator();
 
 const MainStackNavigator = () => {
